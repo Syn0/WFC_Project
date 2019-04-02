a94b524811a43aa0a6abcf10c00020b4bfc38234
Void <florianlassalle78@gmail.com>
2019-04-02 16:04:41 +0200

    multiple layer

--- Wtc/Assets/OverlapWFC.cs
+++ Wtc/Assets/OverlapWFC.cs
@@ -62,7 +62,8 @@ class OverlapWFC : MonoBehaviour{
 	void Awake(){}
 
 	void Start(){
-		Generate();
+		Generate(output);
+        Generate(GameObject.Find("output-overlap2"));
 
     }
 
@@ -72,7 +73,7 @@ class OverlapWFC : MonoBehaviour{
 		}
 	}
 
-	public void Generate() {
+	public void Generate(GameObject outputObj) {
 		if (training == null){Debug.Log("Can't Generate: no designated Training component");}
 		if (IsPrefabRef(training.gameObject)){
 			GameObject o = CreatePrefab(training.gameObject, new Vector3(0,99999f,0f), Quaternion.identity);
@@ -81,26 +82,31 @@ class OverlapWFC : MonoBehaviour{
 		if (training.sample == null){
 			training.Compile();
 		}
-		if (output == null){
+		if (outputObj == null){
 			Transform ot = transform.Find("output-overlap");
-			if (ot != null){output = ot.gameObject;}}
-		if (output == null){
-			output = new GameObject("output-overlap");
-			output.transform.parent = transform;
-			output.transform.position = this.gameObject.transform.position;
-			output.transform.rotation = this.gameObject.transform.rotation;}
-		for (int i = 0; i < output.transform.childCount; i++){
-			GameObject go = output.transform.GetChild(i).gameObject;
+			if (ot != null){outputObj = ot.gameObject;}}
+		if (outputObj == null){
+			outputObj = new GameObject("output-overlap");
+			outputObj.transform.parent = transform;
+			outputObj.transform.position = this.gameObject.transform.position;
+			outputObj.transform.rotation = this.gameObject.transform.rotation;}
+		for (int i = 0; i < outputObj.transform.childCount; i++){
+			GameObject go = outputObj.transform.GetChild(i).gameObject;
 			if (Application.isPlaying){Destroy(go);} else {DestroyImmediate(go);}
 		}
 		group = new GameObject(training.gameObject.name).transform;
-		group.parent = output.transform;
-		group.position = output.transform.position;
-		group.rotation = output.transform.rotation;
+		group.parent = outputObj.transform;
+		group.position = outputObj.transform.position;
+		group.rotation = outputObj.transform.rotation;
         group.localScale = new Vector3(1f, 1f, 1f);
         rendering = new GameObject[width, depth, 3];
 		model = new OverlappingModel(training.sample, N, width, depth, periodicInput, periodicOutput, symmetry, foundation);
         undrawn = true;
+
+        if (model.Run(seed, iterations))
+        {
+            Draw();
+        }
     }
 
 	void OnDrawGizmos(){
@@ -183,7 +189,7 @@ class OverlapWFC : MonoBehaviour{
                     }
 
                 }
-            }
+            }            
         }
     }
 }
@@ -195,7 +201,8 @@ public class WFCGeneratorEditor : Editor {
 		OverlapWFC generator = (OverlapWFC)target;
 		if (generator.training != null){
 			if(GUILayout.Button("generate")){
-				generator.Generate();
+				generator.Generate(generator.output);
+                generator.Generate(GameObject.Find("output-overlap2"));
 			}
 			if (generator.model != null){
 				if(GUILayout.Button("RUN")){
